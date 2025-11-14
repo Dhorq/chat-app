c9b64480a9c4d85fe93821b52a8541a492af91d4
Ridho Rifqy Muhammad <ridhorifqy34@gmail.com>
2025-11-14 23:20:48 +0700

    add the code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Setting from "./pages/Setting";
+import Profile from "./pages/Profile";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route index element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/signup"
+          element={!authUser ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route path="/setting" element={<Setting />} />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
